1d126bda68cd16120bc28a20826af09b0239858e
Sepero <cowpie2000@hotmail.com>
2013-02-27 19:31:58 -0500

    Added logging and shell recognition
    Added script logging, so that a log is kept of scripts that are run.
    
    Added shell recognition, so the location of the shell is discovered.
    
    Removed 'here document' and changed to simple echo of BB_FILE to improve broad compatibility.

--- bb_install.sh
+++ bb_install.sh
@@ -1,9 +1,9 @@
-#!/system/xbin/sh
 # boot-buddy.sh [--uninstall]
 
 # Boot Buddy v1.3
 # Licensed GNU GPL v2, Author Sepero
 
+# Setup global variables.
 ID=$(id | sed 's/[^=]*=//;s/\([0-9]*\).*/\1/')
 BB_DIR="boot_buddy_scripts"
 BB_FILE="/data/boot_buddy.sh"
@@ -13,20 +13,23 @@ IR_MAIN="chown 0.0 $BB_FILE
 chmod 755 $BB_FILE
 busybox sh $BB_FILE"
 IR_TAIL="# bootbuddy boot script end"
+[ -n $EXTERNAL_ADD_STORAGE ] && EXTERNAL_STORAGE=$EXTERNAL_ADD_STORAGE
 
-SHELL="/system/xbin/sh"
-[ -f $SHELL ] || SHELL="/system/bin/sh"
-[ -f $SHELL ] || exit_error "System shell could not be found: $SHELL"
-
+# An Error function incase we need to abort the script.
 exit_error () {
   echo "$@" >&2
 	exit 127
 }
 
+# Check for shell.
+SHELL="/system/xbin/sh"
+[ -f $SHELL ] || SHELL="/system/bin/sh"
+[ -f $SHELL ] || exit_error "System shell could not be found: $SHELL"
+
 # Check for root.
 [ $ID == "0" ] || exit_error "You're not root"
 
-# Check for busy box
+# Check for busybox
 [ -f /system/bin/busybox -o -f /system/xbin/busybox ] || exit_error "Busybox was not found"
 
 # Remount the system as writable.
@@ -41,7 +44,7 @@ if [ "$1" == "--uninstall" ]; then
 		echo "Cleaning $IR_FILE"
 		match=$(sed "/$IR_HEAD/,/$IR_TAIL/d" $IR_FILE)
 		remain=$(echo "$match" | busybox grep -v "^[[:space:]]$")
-		if [ "$remain" == "#!/system/xbin/sh" ]; then
+		if [ "$remain" == "#!$SHELL" ]; then
 			rm $IR_FILE
 		else
 			echo "$match" > $IR_FILE
@@ -54,7 +57,7 @@ fi
 text="$IR_HEAD\n$IR_MAIN\n$IR_TAIL"
 echo "Setting up $IR_FILE"
 if [ ! -e $IR_FILE ]; then
-	busybox echo -e "#!/system/xbin/sh\n" > $IR_FILE
+	busybox echo -e "#!$SHELL\n" > $IR_FILE
 	busybox echo -e "$text" >> $IR_FILE
 else
 	# Remove text if it's already in the file.
@@ -65,31 +68,34 @@ fi
 chown 0.0 $IR_FILE
 chmod 755 $IR_FILE
 
+# Creates or overwrites the file /data/boot_buddy.sh ($BB_FILE)
 echo "Setting up $BB_FILE"
-if [ ! -e $BB_FILE ]; then
-	[ -n $EXTERNAL_ADD_STORAGE ] && EXTERNAL_STORAGE=$EXTERNAL_ADD_STORAGE
-cat <<EOF> $BB_FILE
-#!/system/xbin/sh
+echo "#!$SHELL
+
+BB_LOG=\"/data/boot_buddy.log\"
+rm \$BB_LOG
 
 delay_for_sd () {
 	for i in \$(seq 0 180); do
-		if [ -e "$EXTERNAL_STORAGE/$BB_DIR/" ]; then
+		if [ -e \"$EXTERNAL_STORAGE/$BB_DIR/\" ]; then
 			break
 		fi
 		busybox sleep 1
 	done
-	cd "$EXTERNAL_STORAGE/$BB_DIR/"
+	cd \"$EXTERNAL_STORAGE/$BB_DIR/\"
 	ls * | sort | while read i; do
-		sh "\$i"
+		echo \"RUNNING \$i:\" >> \$BB_LOG
+		busybox sh \"\$i\" >> \$BB_LOG 2>> \$BB_LOG
+		echo \"\" >> \$BB_LOG
 	done
 }
 
-delay_for_sd&
-EOF
-fi
+delay_for_sd&" > $BB_FILE
+# Finished writing the file $BB_FILE
 
-echo "Creating folder $EXTERNAL_STORAGE/$BB_DIR"
-mkdir "$EXTERNAL_STORAGE/$BB_DIR"
+
+echo "Setting up folder $EXTERNAL_STORAGE/$BB_DIR"
+mkdir "$EXTERNAL_STORAGE/$BB_DIR" 2> /dev/null
 
 busybox echo -e "\nPut scripts you want to run at boot in the folder $BB_DIR"
 busybox echo -e  "\nFinished"
